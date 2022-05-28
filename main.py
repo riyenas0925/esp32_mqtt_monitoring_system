b832e19de424ffeb5d7bbcf05eee686434e80f36
riyenas0925 <riyenas0925@gmail.com>
2022-05-28 16:33:13 +0900

    feat: ESP32 네트워크 연결 기능 구현

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-from time import sleep_ms
-from machine import Pin
+# wifi config
+wifi_ssid = "iPhone"
+wifi_password = "testmqtt"
+
+
+def connect():
+    import network
+
+    station = network.WLAN(network.STA_IF)
+
+    if station.isconnected():
+        print("Already connected")
+        return
+
+    station.active(True)
+    station.connect(wifi_ssid, wifi_password)
+
+    while not station.isconnected():
+        pass
+
+    print("Connection successful")
+    print(station.ifconfig())
 
 
 def main():
-    led = Pin(16, Pin.OUT)
-
-    while True:
-        led.on()
-        sleep_ms(500)
-        led.off()
-        sleep_ms(500)
+    # connect Wi-Fi network
+    connect()
 
 
 if __name__ == '__main__':
