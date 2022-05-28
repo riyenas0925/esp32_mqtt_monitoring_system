18aa9e70741a7dee4fcd7f8ca6317f4c75d3f5ae
riyenas0925 <riyenas0925@gmail.com>
2022-05-28 16:37:24 +0900

    feat: MQTT broker에게 센서 정보를 발행 하도록 구현
    * 센서는 lux, temp, humi로 우선은 더미 데이터를 넣어 테스트한다.
    * topic은 sensors/1로 우선 설정

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-# wifi config
+from time import sleep
+from umqtt.simple import MQTTClient
+
+# Wi-Fi config
 wifi_ssid = "iPhone"
 wifi_password = "testmqtt"
+
+# mqtt config
+mqtt_server_host = "rabbitmq.riyenas.dev"
+mqtt_client_id = "mqtt_client"
+mqtt_topic = b"sensors/1"
 
 
 def connect():
@@ -25,6 +33,29 @@ def connect():
 def main():
     # connect Wi-Fi network
     connect()
+
+    # connect mqtt broker
+    client = MQTTClient(mqtt_client_id, mqtt_server_host)
+    client.connect()
+
+    # publish message
+    while True:
+        try:
+            sleep(10)
+
+            # dummy data
+            humidity = 50.0
+            temperature = 36.5
+            lux = 1000
+
+            json = b'{"id": %u, "humidity": %.1f, "temperature": %.1f, "lux": %.1f}' % (1, humidity, temperature, lux)
+
+            client.publish(mqtt_topic, json)
+            print(mqtt_topic, json)
+        except OSError as e:
+            print("error: {0}".format(e))
+
+    client.disconnect()
 
 
 if __name__ == '__main__':
